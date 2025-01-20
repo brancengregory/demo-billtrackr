52ff792ea09fd4979d048dbf14df9ab73536ca3a
AnthonyOKC <anthonyfloresokc@gmail.com>
2025-01-20 16:13:57 -0600

    Add scorecards and bill state progress functions

--- R/main.R
+++ R/main.R
@@ -132,3 +132,133 @@ bill_sheet_bills <- function(bill_sheet_id) {
   return(bills)
 }
 
+#' Get information about scorecards
+#'
+#' This function retrieves information about all scorecards for the authenticated user.
+#'
+#' @return A tibble of scorecards containing information like scorecardID, name, categories, etc.
+#'
+#' @examples
+#' scorecards()
+#'
+#' @export
+scorecards <- function() {
+  scorecards <- billtrack_req("scorecards") |>
+    purrr::pluck("scorecards") |>
+    tibble::as_tibble()
+
+  return(scorecards)
+}
+
+#' Get bills from a specific scorecard
+#'
+#' This function retrieves all bills contained in a specified scorecard.
+#'
+#' @param scorecard_id The unique identifier of the scorecard
+#' @return A tibble of bills contained in the specified scorecard
+#'
+#' @examples
+#' scorecard_bills(12345)
+#'
+#' @export
+scorecard_bills <- function(scorecard_id) {
+  bills <- billtrack_req(glue::glue("scorecards/{scorecard_id}/bills")) |>
+    purrr::pluck("bills") |>
+    tibble::as_tibble()
+
+  return(bills)
+}
+
+#' Get legislator information for a specific scorecard
+#'
+#' This function retrieves information about a specific legislator in a scorecard.
+#'
+#' @param scorecard_id The unique identifier of the scorecard
+#' @param legislator_id The unique identifier of the legislator
+#' @return A tibble containing legislator information and category scores
+#'
+#' @examples
+#' scorecard_legislator(12345, 18718)
+#'
+#' @export
+scorecard_legislator <- function(scorecard_id, legislator_id) {
+  legislator <- billtrack_req(glue::glue("scorecards/{scorecard_id}/legislators/{legislator_id}")) |>
+    purrr::pluck("legislator") |>
+    tibble::as_tibble()
+
+  return(legislator)
+}
+
+#' Get legislator votes for a specific scorecard
+#'
+#' This function retrieves voting information for a specific legislator in a scorecard.
+#'
+#' @param scorecard_id The unique identifier of the scorecard
+#' @param legislator_id The unique identifier of the legislator
+#' @return A tibble containing voting information
+#'
+#' @examples
+#' scorecard_legislator_votes(12345, 18718)
+#'
+#' @export
+scorecard_legislator_votes <- function(scorecard_id, legislator_id) {
+  votes <- billtrack_req(glue::glue("scorecards/{scorecard_id}/legislators/{legislator_id}/votes")) |>
+    purrr::pluck("billVotes") |>
+    tibble::as_tibble()
+
+  return(votes)
+}
+
+#' Get all legislators for a specific scorecard
+#'
+#' This function retrieves information about all legislators in a scorecard.
+#'
+#' @param scorecard_id The unique identifier of the scorecard
+#' @param category_id Optional category ID to filter legislation
+#' @return A tibble containing legislator information
+#'
+#' @examples
+#' scorecard_legislators(12345)
+#' scorecard_legislators(12345, category_id = 67)
+#'
+#' @export
+scorecard_legislators <- function(scorecard_id, category_id = NULL) {
+  path <- if (is.null(category_id)) {
+    glue::glue("scorecards/{scorecard_id}/legislators")
+  } else {
+    glue::glue("scorecards/{scorecard_id}/legislators?category={category_id}")
+  }
+
+  legislators <- billtrack_req(path) |>
+    purrr::pluck("legislators") |>
+    tibble::as_tibble()
+
+  return(legislators)
+}
+
+#' Get bill progress information by state
+#'
+#' This function retrieves information about bill progress in specified states.
+#'
+#' @param state_codes Optional state codes to filter progress information
+#' @return A tibble containing bill progress information
+#'
+#' @examples
+#' state_bill_progress()
+#' state_bill_progress("CA")
+#'
+#' @export
+state_bill_progress <- function(state_codes = NULL) {
+  path <- if (is.null(state_codes)) {
+    "state/bill-progress"
+  } else {
+    glue::glue("state/bill-progress?stateCode={state_codes}")
+  }
+
+  progress <- billtrack_req(path) |>
+    purrr::pluck("billProgress") |>
+    tibble::as_tibble()
+
+  return(progress)
+}
+
